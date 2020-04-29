3731364ef919fb4b59db8664e700baf0875c67bd
GitLab <4402808-jeidoz@users.noreply.gitlab.com>
2020-04-29 03:06:45 +0300

    Add project files.

---
+++ DevSilenceKeeperBot/DevSilenceKeeper.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using File = System.IO.File;
+
+namespace DevSilenceKeeperBot
+{
+    internal sealed class DevSilenceKeeper
+    {
+        private const char WordsSplitter = ';';
+        private readonly ITelegramBotClient _bot;
+        private readonly string _wordsFilePath;
+        private List<string> _forbiddenWords;
+
+        public DevSilenceKeeper(string token, string wordsFilePath = "words.txt")
+        {
+            _bot = new TelegramBotClient(token);
+            _wordsFilePath = wordsFilePath;
+            _forbiddenWords = File.ReadAllText(wordsFilePath)
+                .Split(new char[] { WordsSplitter }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            _bot.OnMessage += OnMessage;
+        }
+
+        private async void OnMessage(object sender, MessageEventArgs e)
+        {
+            if(string.IsNullOrEmpty(e.Message.Text))
+            {
+                return;
+            }
+
+            string replyMessage = default;
+            if (IsCommand(e.Message.Text))
+            {
+                Console.WriteLine($"{GetUserFullName(e.Message.From)} запросил команду: {e.Message.Text}");
+                replyMessage = await GetCommandResponse(e.Message);
+            }
+            else if (IsContainsForbiddenWord(e.Message.Text))
+            {
+                if (!await IsAdmin(e.Message.From, e.Message.Chat.Id))
+                {
+                    replyMessage = $"Пользователь {GetUserFullName(e.Message.From)} нарушил второе правило чата!";
+                    DateTime until = DateTime.Now.AddSeconds(31);
+                    await _bot.KickChatMemberAsync(
+                        chatId: e.Message.Chat.Id, 
+                        userId: e.Message.From.Id, 
+                        untilDate: until);
+                }
+                else
+                {
+                    replyMessage = $"Модератор {GetUserFullName(e.Message.From)} нарушает второе правило чата!";
+                }
+            }
+
+            if (!string.IsNullOrEmpty(replyMessage))
+            {
+                try
+                {
+                    await _bot.SendTextMessageAsync(
+                        chatId: e.Message.Chat.Id,
+                        text: replyMessage,
+                        replyToMessageId: e.Message.MessageId);
+
+                    Console.WriteLine(replyMessage);
+                }
+                catch(ApiRequestException)
+                {
+                    replyMessage = "Мышь успела удалить сообщение для reply\n(╯ರ ~ ರ)╯︵ ┻━┻.";
+                    await _bot.SendTextMessageAsync(
+                        chatId: e.Message.Chat.Id,
+                        text: replyMessage);
+                }
+            }
+        }
+        private bool IsCommand(string text)
+        {
+            return text.StartsWith('/');
+        }
+        private async Task<string> GetCommandResponse(Message message)
+        {
+            string[] words = message.Text
+                .Replace("@devsilencekeeper_bot", string.Empty)
+                .Substring(1)
+                .Split();
+            string command = words.First();
+            string args = words.Length > 1 ? words[1] : null;
+            string response;
+            switch (command)
+            {
+                case "words":
+                case "templates":
+                    string templates = string.Join('\n', _forbiddenWords);
+                    response = $"Cтроки-шаблоны в банлисте:\n{templates}";
+                    break;
+                case "add":
+                    if(!await IsAdmin(message.From, message.Chat.Id))
+                    {
+                        response = "Добавлять строки-шаблоны могут только модераторы!";
+                        break;
+                    }
+
+                    if (string.IsNullOrWhiteSpace(args) ||  args.Length < 4)
+                    {
+                        response = "Строка-шаблон должна состоять минимум из 4ех символов!";
+                        break;
+                    }
+
+                    if (_forbiddenWords.Contains(args))
+                    {
+                        response = "Дананя строка-шаблон уже присуствует в банлисте.";
+                        break;
+                    }
+
+                    File.AppendAllText(_wordsFilePath, $"{args}{WordsSplitter}");
+                    _forbiddenWords.Add(args);
+                    response = $"Строка-шаблон \"{args}\" успешно добавлена";
+                    break;
+                case "remove":
+                case "delete":
+                case "del":
+                case "rm":
+                    if (!await IsAdmin(message.From, message.Chat.Id))
+                    {
+                        response = "Удалять строки-шаблоны могут только модераторы!";
+                        break;
+                    }
+
+                    if (string.IsNullOrWhiteSpace(args) || args.Length < 4)
+                    {
+                        response = "Строка-шаблон должна состоять минимум из 4ех символов!";
+                        break;
+                    }
+
+                    if (!_forbiddenWords.Contains(args))
+                    {
+                        response = "Дананя строка-шаблон отсуствует в банлисте.";
+                        break;
+                    }
+
+                    StringBuilder fileContent = new StringBuilder(File.ReadAllText(_wordsFilePath));
+                    fileContent.Replace($"{args}{WordsSplitter}", string.Empty);
+                    File.WriteAllText(_wordsFilePath, fileContent.ToString());
+
+                    _forbiddenWords.Remove(args);
+                    response = $"Строка-шаблон \"{args}\" успешно убрана";
+                    break;
+                case "help":
+                default:
+                    response = string.Format("Список комманд:\n{0}\n{1}\n{2}\n{3}\n{4}\n{5}",
+                        "/help – показать помощь",
+                        "/words – показать запрещенные строки-шаблоны",
+                        "/templates – альтернатива /words",
+                        "/add – добавить запрещенную строку-шаблон",
+                        "/remove (/rm) – убрать запрещенную строку-шаблон",
+                        "/delete (/del) – альтернатива /remove");
+                    break;
+            }
+
+            return response;
+        }
+        private bool IsContainsForbiddenWord(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return false;
+
+            return _forbiddenWords.Any(word => text.Contains(word));
+        }
+        private async Task<bool> IsAdmin(User sender, long chatId)
+        {
+            var chatAdmins = await _bot.GetChatAdministratorsAsync(chatId);
+            return chatAdmins.Any(member => member.User.Id == sender.Id);
+        }
+        private string GetUserFullName(User user)
+        {
+            StringBuilder builder = new StringBuilder();
+            if (!string.IsNullOrWhiteSpace(user.FirstName))
+            {
+                builder.Append(user.FirstName);
+            }
+
+            if (!string.IsNullOrWhiteSpace(user.LastName))
+            {
+                builder.Append($" {user.LastName}");
+            }
+
+            if (!string.IsNullOrWhiteSpace(user.Username))
+            {
+                builder.Append($" (@{user.Username})");
+            }
+
+            return builder.ToString();
+        }
+
+        public void StartPolling()
+        {
+            _bot.StartReceiving();
+            Console.WriteLine("Бот начал обрабатывать сообщения...");
+        }
+        public void StopPolling()
+        {
+            _bot.StopReceiving();
+            Console.WriteLine("Бот прекратил работу...");
+        }
+    }
+}
---
+++ DevSilenceKeeperBot/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace DevSilenceKeeperBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DevSilenceKeeper bot;
+                if (args.Length == 1)
+                {
+                    bot = new DevSilenceKeeper(args[0]);
+                }
+                else if (args.Length == 2)
+                {
+                    bot = new DevSilenceKeeper(args[0], args[1]);
+                }
+                else
+                {
+                    Console.WriteLine("Для запуска бота задайте параметр токена (и опционально путь к файлу с запрещенных слов)");
+                    Console.WriteLine("./DevSilenceKeeper 012345:abcdf [words.txt]");
+                    return;
+                }
+
+                bot.StartPolling();
+                Console.WriteLine("Введите \"stop\" что бы остановить бота.");
+                do
+                {
+                    if (Console.ReadLine() == "stop")
+                        break;
+                }
+                while (true);
+                bot.StopPolling();
+            }
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("Не удалось найти файл из запрещенными словами.");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Не удалось найти файл из запрещенными словами.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Во время роботы случилась ошибка:");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                return;
+            }
+        }
+    }
+}
