bc85eed30924ae4379b673b96473f28420015c5a
GitLab <4402808-jeidoz@users.noreply.gitlab.com>
2020-06-14 18:52:20 +0300

    Implemented extension methods instead of simple methods

---
+++ DevSilenceKeeperBot/Extensions/TextExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DevSilenceKeeperBot.Extensions
+{
+    public static class TextExtensions
+    {
+        public static string RemoveSpecialCharacters(this string text)
+        {
+            var pattern = new Regex("[-:!@#$%^&*()}{|\":?><\\[\\]\\;'/.,~]");
+            return pattern.Replace(text, "").ToLower();
+        }
+    }
+}
---
+++ DevSilenceKeeperBot/Extensions/UserExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace DevSilenceKeeperBot.Extensions
+{
+    public static class UserExtensions
+    {
+        public static async Task<bool> IsAdmin(this User sender, long chatId, TelegramBotClient botClient)
+        {
+            var chatAdmins = await botClient.GetChatAdministratorsAsync(chatId);
+            return chatAdmins.Any(member => member.User.Id == sender.Id);
+        }
+    }
+}
