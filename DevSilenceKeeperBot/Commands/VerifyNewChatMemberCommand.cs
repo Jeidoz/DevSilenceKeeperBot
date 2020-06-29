1ef44bcb505ded17e4448bcf74bdd3d6e3a415d1
GitLab <23425714+Jeidoz@users.noreply.github.com>
2020-06-29 19:16:27 +0300

    Fixed unmute after rejoin to chat.

--- DevSilenceKeeperBot/Commands/VerifyNewChatMemberCommand.cs
+++ DevSilenceKeeperBot/Commands/VerifyNewChatMemberCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using DevSilenceKeeperBot.Extensions;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
@@ -27,12 +28,24 @@ namespace DevSilenceKeeperBot.Commands
 
         public override async Task Execute(Message message, TelegramBotClient botClient)
         {
+            if (await message.From.IsMuted(message.Chat.Id, botClient))
+            {
+                var chatMember = await botClient.GetChatMemberAsync(message.Chat.Id, message.From.Id);
+                string replyText =
+                    $"С возвращением в чат! Ты ещё в муте до {chatMember.UntilDate:dd.MM.yyyy HH:mm:ss} (UTC+02:00)";
+                await botClient.SendTextMessageAsync(
+                    message.Chat.Id,
+                    replyText,
+                    replyToMessageId: message.MessageId);
+                return;
+            }
+            
             await botClient.RestrictChatMemberAsync(
                 message.Chat.Id,
                 message.From.Id,
                 new ChatPermissions {CanSendMessages = false},
                 DateTime.Now).ConfigureAwait(false);
-            
+
             var captchaMarkup = new InlineKeyboardMarkup(
                 new[]
                 {
